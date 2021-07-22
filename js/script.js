3eb90adf231abf4ec9674d3147e6d6dc7040ca97
Alessandro2014 <alessandromerenda22@gmail.com>
2021-07-22 15:48:51 +0200

    info generiche carta

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const carta = {
+    id = 1,
+    nome: 'Bloodfire Colossus',
+    costoLancio: ['6', 'R', 'R'],
+    costoManaCombinato: 8,
+    tipoCarta: 'Creatura',
+    specTipoCarta: 'Giant',
+    espansione: {
+        
+    }
+}
