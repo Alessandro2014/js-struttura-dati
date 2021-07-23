0459719ff94c733f2179fe2843a00d0cc320cff8
Alessandro2014 <alessandromerenda22@gmail.com>
2021-07-23 17:27:21 +0200

    stampa in pagine, errore riga 130

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-const carta = {     //dichiarazione primo oggetto
+const mazzo = [
+{
     id: 1,
     nome: 'Bloodfire Colossus',
     costoLancio: ['6', 'R', 'R'],
@@ -37,12 +38,132 @@ const carta = {     //dichiarazione primo oggetto
         sorgente: '/img.jpg',
     },
     coloreSfondo:'red',
-};
+},
+{
+    id: 1,
+    nome: 'Bloodfire Colossus',
+    costoLancio: ['6', 'R', 'R'],
+    costoManaCombinato: 8,
+    tipoCarta: 'Creatura',
+    specTipoCarta: 'Giant',
+    espansione: {       // innesto oggetto per espansioni
+        nEdizione: 9,
+        rarita: 'oro',
+        collezioneNum: 12,
+        },
+    testoDiColore: {        // innesto oggetto per infotesto
+        dichiarazioni: 'Anaba Shaman...',
+        autore:'Gog di Magog',
+    },
+    abilita: [          // innesto array abilità
+        {
+        costoLancio: ['R', 'T'],
+        descrizione: 'debole',  
+        },
+        {
+        costoLancio: ['W', 'B', 'T'],
+        descrizione: 'medio',    
+        },
+    ],
+    numeroCollezione: '177/350',
+    costituzione: 2,
+    forza: 5,
+    coloreBordo: 'black',
 
-console.log(carta);
+    illustrazione: {    // innesto oggetto per autore
+        autore: {
+            id: 1,
+            nome: 'Ron Spears',
+        },
+        sorgente: '/img.jpg',
+    },
+    coloreSfondo:'red',
+},
+{
+    id: 1,
+    nome: 'Bloodfire Colossus',
+    costoLancio: ['6', 'R', 'R'],
+    costoManaCombinato: 8,
+    tipoCarta: 'Creatura',
+    specTipoCarta: 'Giant',
+    espansione: {       // innesto oggetto per espansioni
+        nEdizione: 9,
+        rarita: 'oro',
+        collezioneNum: 12,
+        },
+    testoDiColore: {        // innesto oggetto per infotesto
+        dichiarazioni: 'Anaba Shaman...',
+        autore:'Gog di Magog',
+    },
+    abilita: [          // innesto array abilità
+        {
+        costoLancio: ['R', 'T'],
+        descrizione: 'debole',  
+        },
+        {
+        costoLancio: ['W', 'B', 'T'],
+        descrizione: 'medio',    
+        },
+    ],
+    numeroCollezione: '177/350',
+    costituzione: 2,
+    forza: 5,
+    coloreBordo: 'black',
+
+    illustrazione: {    // innesto oggetto per autore
+        autore: {
+            id: 1,
+            nome: 'Ron Spears',
+        },
+        sorgente: '/img.jpg',
+    },
+    coloreSfondo:'red',
+},
+{
+    id: 1,
+    nome: 'Bloodfire Colossus',
+    costoLancio: ['6', 'R', 'R'],
+    costoManaCombinato: 8,
+    tipoCarta: 'Creatura',
+    specTipoCarta: 'Giant',
+    espansione: {       // innesto oggetto per espansioni
+        nEdizione: 9,
+        rarita: 'oro',
+        collezioneNum: 12,
+        },
+    testoDiColore: {        // innesto oggetto per infotesto
+        dichiarazioni: 'Anaba Shaman...',
+        autore:'Gog di Magog',
+    },
+    abilita: [          // innesto array abilità
+        {
+        costoLancio: ['R', 'T'],
+        descrizione: 'debole',  
+        },
+        {
+        costoLancio: ['W', 'B', 'T'],
+        descrizione: 'medio',    
+        },
+    ],
+    numeroCollezione: '177/350',
+    costituzione: 2,
+    forza: 5,
+    coloreBordo: 'black',
+
+    illustrazione: {    // innesto oggetto per autore
+        autore: {
+            id: 1,
+            nome: 'Ron Spears',
+        },
+        sorgente: '/img.jpg',
+    },
+    coloreSfondo:'red',
+},
+];
+
 
             //* FUNZIONE PREDISPOSIZIONE STAMPA
-const cartaTemplate = creaCartaTemplate(carta);
+const cartaTemplate = creaCartaTemplate(mazzo);
 
 function creaCartaTemplate (carta) {
     const specTipoCarta = carta.specTipoCarta ? ` ${carta.specTipoCarta}` :' ';
@@ -65,14 +186,12 @@ const cartaTemplate = `
     <li>Costo di Lancio: ${carta.costoLancio.join(' ')}</li>
     <li>Costo mana combinato: ${carta.costoManaCombinato}</li>
     <li>Tipo carta: ${carta.specTipoCarta}</li>
-
     <li><strong>Espansione:</strong></li>
 <ul>
         <li>Stampa Edizione: ${carta.espansione.nEdizione}</li>
         <li>Rarità:: ${carta.espansione.rarita}</li>
         <li>Collezione Numero: ${carta.espansione.collezioneNum}</li>
 </ul>
-
     <li><strong>Colore di Testo:</strong></li>
         <li>Dichiarazioni: ${carta.testoDiColore.dichiarazioni}</li>
         <li>Autore: ${carta.testoDiColore.autore}</li>
@@ -92,9 +211,15 @@ const cartaTemplate = `
     return cartaTemplate;
 }
 
-
+        //* STAMPA IN PAGINA
+let mazzoTemplate = ' ';
+for (let i = 0; i < mazzo.length; i++){
+    const singolaCarta = mazzo[i];
+    singolaCartaTemplate += creaCartaTemplate(singolaCarta);
+    mazzoTemplate += singolaCarta;
+}
 
 
 const sezioneCarta = document.getElementById('carta');
 
-sezioneCarta.innerHTML = cartaTemplate;
+sezioneCarta.innerHTML = mazzoTemplate;
